6c0d73381cf530e070c573cb2e9e36efa3cf01ad
Yusuf <yyusbel@yahoo.com>
2021-12-15 00:23:25 +0100

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -2,9 +2,9 @@ const repeatString = function(hey, times) {
     if (times<0) return `ERROR`
     let string=``;
     for(let i=0; i<times; i++) {
-        string+=`hey`
+        string+=hey
     }
-    return String
+    return string
 };
 
 // Do not edit below this line
