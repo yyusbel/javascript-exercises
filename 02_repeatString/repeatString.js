bc843f2483968c33af871115044377cef9314a91
Yusuf <yyusbel@yahoo.com>
2021-12-15 01:15:33 +0100

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,11 +1,10 @@
-const repeatString = function(hey, times) {
-    if (times<0) return `ERROR`
-    let string=``;
-    for(let i=0; i<times; i++) {
-        string+=hey
+const repeatString = function(word, times) {
+    if (times < 0) return 'ERROR'
+    let string = ''
+    for (let i = 0; i < times; i++) {
+      string += word
     }
     return string
-};
-
-// Do not edit below this line
-module.exports = repeatString;
+  };
+  
+  module.exports = repeatString;
